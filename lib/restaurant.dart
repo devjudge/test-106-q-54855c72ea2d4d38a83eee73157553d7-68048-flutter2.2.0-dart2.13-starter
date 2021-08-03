a5ebc71c8a97d91ac7458cea07998e18184224d5
JayapradhaSaravanan <87609817+JayapradhaSaravanan@users.noreply.github.com>
2021-08-03 13:07:31 +0530

    Create restaurant.dart

---
+++ lib/restaurant.dart
@@ -0,0 +1,13 @@
+
+
+class Restaurant {
+  final double rating;
+  final String title;
+  final String logoImage;
+  
+  Restaurant({
+    this.rating,
+    this.title,
+    this.logoImage,
+  });
+}
